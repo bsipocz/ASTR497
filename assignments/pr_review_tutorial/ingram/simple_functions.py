f245ef6f034616fce900e3f6d2b078c60e19cc63
thunderboltrmi <thunder.boltrmi@gmail.com>
2021-04-23 14:16:48 -0700

    While loop fixed.

--- assignments/pr_review_tutorial/ingram/simple_functions.py
+++ assignments/pr_review_tutorial/ingram/simple_functions.py
@@ -1,7 +1,8 @@
 def fibonacci(max):
     values = [0, 1]
+    iter = 0
     while values[0] + values[1] < max:
-        values.append(values[0] + values[1])
+        values.append(values[iter] + values[iter+=1])
     return values
 
 
