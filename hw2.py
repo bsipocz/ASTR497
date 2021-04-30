e4e109e4a9be121df5fb3e14776d3790d777e939
maria8ch <maria8ch@gmail.com>
2021-04-30 16:05:49 -0700

    set up hw 2 branch, start hw

---
+++ hw2.py
@@ -0,0 +1,5 @@
+#Challenge
+#1. Examine the headers of the first and second HDU in the point source catalog and try adding new keywords to them or changing them
+#2. Make a histogram of the G-band magnitude of the sources in the catalog - can you figure out what the upper limit for Gmag was when the table was selected?
+#3. Make a plot of the position of the sources on the sky in the point source catalog, in Galactic coordinates l and b (you will need to use what we learned about SkyCoord in the previous tutorial)
+#4. Try and produce a new FITS file that contains the image in the primary HDU and the above table in the second HDU
